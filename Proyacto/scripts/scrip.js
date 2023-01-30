c23a4008460d8eb77b02127a4e747d3a27291993
Fleitas Mathias <matu.cunha@gmail.com>
2023-01-29 23:49:59 -0300

    Primera prentegra codigo final

--- Proyacto/scripts/scrip.js
+++ Proyacto/scripts/scrip.js
@@ -14,22 +14,43 @@ function compra_de_moneda(moneda,monto){
         let conversion=monto/9;
          return conversion;
      }
+}     
+function venta_de_moneda(moneda,monto){
+        if(moneda=="usd"){
+           let conversion = monto*40;
+            return conversion;
+        }
+        else if(moneda=="ar"){
+            let conversion=monto*0.35;
+             conversion=parseFloat(conversion);
+             return conversion;
+         }
+        else if(moneda=="br"){
+            let conversion=monto*9;
+             return conversion;
+         }
 
 }
-let moneda = prompt("Ingrese: usd (para dolares), ar (para pesos argentinos), br (para reales) o salir");
+let compra_o_venta = prompt("Ingrese si quiere: comprar , vender o salir:");
 
-while(moneda!="salir"){
+while(compra_o_venta!="salir"){
+    let moneda = prompt("Ingrese: usd (para dolares), ar (para pesos argentinos), br (para reales)");
     let monto = parseInt(prompt("Ingrese un monto a convertir"));
-    let conversion= compra_de_moneda(moneda,monto);
-    if(typeof(monto)=="number"){
+    if(typeof(monto)=="number"&&compra_o_venta=="comprar"){
+        let conversion= compra_de_moneda(moneda,monto);
         console.log("Monto ingresado: ",monto);
         console.log("El valro de la conversion es:",conversion);
-        moneda = prompt("Ingrese: usd (para dolares), ar (para pesos argentinos), br (para reales) o salir");
+        compra_o_venta = prompt("Ingrese si quiere: compra , vender o salir:");
         
     }
+    else if(typeof(monto)=="number"&&compra_o_venta=="vender"){
+        let conversion= venta_de_moneda(moneda,monto);
+        console.log("Monto ingresado: ",monto);
+        console.log("El valro de la conversion es:",conversion);
+        compra_o_venta = prompt("Ingrese si quiere: compra , vender o salir:");
 
-}  
-
-if(moneda == "salir"){
-    console.log("Gracias por utilizar el convertor");
-}  
+    }
+}
+if(compra_o_venta == "salir"){
+    console.log("Gracias por utilizar el convertidor");
+}
